a75c842461ac55d5bfb303a9c67fe057a66a46c1
hui <hui@VMW10>
2016-10-12 21:53:00 +0800

    promise; handlebars;

--- Will.Files/routes/file.js
+++ Will.Files/routes/file.js
@@ -1,76 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../config.json');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
 
 /* GET one file. */
 router.get('/*', function(req, res, next) {
 	var relatedPath = decodeURI(req.path);
-	var path = config.filePath + relatedPath
+	var absolutePath = config.filePath + relatedPath
 
-	var fs = require('fs');
-	fs.exists(path, function (exists) {
-		if (exists) {
-			fs.stat(path, function (err, stats) {
-				if (err) {
-					res.status(500).send('internal error. ' + err);
-				} else if (stats.isFile()) {
-					fs.readFile(path, function (err, data) {
-						res.type('text').send(data.toString());
-					});
-				} else if (stats.isDirectory()) {
-					fs.readdir(path, function (err, files) {
-						var output = new Array();
-						//var e;
+	fs.existsAsync(absolutePath).then(function () {
+		res.status(404).send('"' + relatedPath + '" not found.')
+	}, function () {
+		fs.statAsync(absolutePath).then(function (stats) {
+			if (stats.isFile()) {
+				fs.readFileAsync(absolutePath).then(function (data) {
+					res.type('text').send(data.toString());
+				});
+			} else if (stats.isDirectory()) {
+				fs.readdirAsync(absolutePath).then(function (files) {
+					var output = new Array();
 
-
-						files.forEach(function (file) {
-
-
-
-							var stats = fs.statSync(file);
-							if (stats.isFile()) {
-								output.push({ name: file, a: 'f' });
-							} else if (stats.isDirectory()) {
-								output.push({ name: file, a: 'd' });
-							}
-
-							/*
-							fs.stat(file, function (err, stats) {
-								if (err) {
-									e = err;
-								} else if (stats.isFile()) {
-									output.push({ name: file, a: 'f' });
-								} else if (stats.isDirectory()) {
-									output.push({ name: file, a: 'd' });
-								}
-							});
-							*/
-						});
-
-						es.json(output);
-
-						/*
-						if (e) {
-							res.status(500).send('internal error. ' + e);
-						} else {
-							res.json(output);
+					files.forEach(function (file) {
+						var fileStats = fs.statSync(path.join(absolutePath, file));
+						if (fileStats.isFile()) {
+							output.push({ p: path.join(relatedPath, file), a: 'f' });
+						} else if (fileStats.isDirectory) {
+							output.push({ p: path.join(relatedPath, file), a: 'd' });
 						}
-						*/
 					});
-				}
-			});
-		} else {
-			res.status(404).send('"' + relatedPath + '" not found.')
-		}
+
+					res.json(output);
+				});
+			}
+		});
+	}).catch(function (e) {
+		res.status(500).send('internal error. ' + e);
 	});
+
 });
 
 module.exports = router;
 
 
 
-const Promise = require('bluebird');
-
-fs.statAsnyc().then(function(stats){}).error(function(){
-
-})
