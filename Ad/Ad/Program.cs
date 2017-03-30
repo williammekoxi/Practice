675cc0ee49460fcbd4ba8b033293f1ae87da34a3
hui <hui@VMW10>
2017-03-30 17:58:50 +0800

    [Practice] AD, .net core

---
+++ Ad/Ad/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var properties = new List<string> { "name", "givenName", "samaccountname", "mail", };
+            var entry = new DirectoryEntry("LDAP://huazhu.com");
+            var searcher = new DirectorySearcher(entry)
+            {
+                Filter = "(samaccountname=huangshenghui)",
+            };
+            properties.ForEach(p => searcher.PropertiesToLoad.Add(p));
+            var result = searcher.FindOne();
+            if (result != null)
+            {
+                properties.ForEach(p => result.Properties[p].Cast<ResultPropertyValueCollection>().ToList().ForEach(values => values.Cast<object>().ToList().ForEach(value => Console.WriteLine("{0}: {1}", p, value))));
+            }
+        }
+    }
+}
